4eef815cb7c3cfa17db3ce166849a7c344917f26
SpyGlass <spyglassinyourass@gmail.com>
2019-03-09 01:28:10 +0300

    Add project files.

---
+++ La2Bot/Helper.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace La2Bot
+{
+    internal static class Helper
+    {
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        internal static extern bool SendMessage(IntPtr hWnd, Int32 msg, Int32 wParam, Int32 lParam);
+        static Int32 WM_SYSCOMMAND = 0x0112;
+        static Int32 SC_RESTORE = 0xF120;
+
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowRect(IntPtr hWnd, ref Rect lpRect);
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Rect
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+        
+        public static Bitmap SaveScreenshot(IntPtr hWnd)
+        {
+            SetForegroundWindow(hWnd);
+            SendMessage(hWnd, WM_SYSCOMMAND, SC_RESTORE, 0);
+
+            var rect = new Rect();
+            GetWindowRect(hWnd, ref rect);
+
+            int width = rect.right - rect.left;
+            int height = rect.bottom - rect.top;
+
+            var bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            Graphics graphics = Graphics.FromImage(bmp);
+            graphics.CopyFromScreen(rect.left, rect.top, 0, 0, new Size(width, height), CopyPixelOperation.SourceCopy);
+            graphics.FillRectangle(Brushes.Black, 330, 230, 150, 100);
+            graphics.FillRectangle(Brushes.Black, 0, 330, 180, 20);
+            graphics.FillRectangle(Brushes.Black, 0, 370, 2000, 2000);
+            graphics.FillRectangle(Brushes.Black, 0, 0, 2000,30);
+
+            var grey = MakeGrayscale3(bmp);
+
+            bmp.Save("test.png", System.Drawing.Imaging.ImageFormat.Png);
+            return grey;
+        }
+
+        public static Bitmap GetHp(IntPtr hWnd)
+        {
+            SetForegroundWindow(hWnd);
+            SendMessage(hWnd, WM_SYSCOMMAND, SC_RESTORE, 0);
+
+            var rect = new Rect();
+            GetWindowRect(hWnd, ref rect);
+
+            var bmp = new Bitmap(150, 5, PixelFormat.Format32bppArgb);
+            Graphics graphics = Graphics.FromImage(bmp);
+            graphics.CopyFromScreen(rect.left + 26, rect.top + 400, 0, 0, new Size(150, 5), CopyPixelOperation.SourceCopy);
+
+            var grey = MakeGrayscale3(bmp);
+
+            bmp.Save("hp.png", System.Drawing.Imaging.ImageFormat.Png);
+            return grey;
+        }
+
+        private static Bitmap MakeGrayscale3(Bitmap original)
+        {
+            //create a blank bitmap the same size as original
+            Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+
+            //get a graphics object from the new image
+            Graphics g = Graphics.FromImage(newBitmap);
+
+            //create the grayscale ColorMatrix
+            ColorMatrix colorMatrix = new ColorMatrix(
+                new float[][]
+                {
+                    new float[] {.3f, .3f, .3f, 0, 0},
+                    new float[] {.59f, .59f, .59f, 0, 0},
+                    new float[] {.11f, .11f, .11f, 0, 0},
+                    new float[] {0, 0, 0, 1, 0},
+                    new float[] {0, 0, 0, 0, 1}
+                });
+
+            //create some image attributes
+            ImageAttributes attributes = new ImageAttributes();
+
+            //set the color matrix attribute
+            attributes.SetColorMatrix(colorMatrix);
+
+            //draw the original image on the new image
+            //using the grayscale color matrix
+            g.DrawImage(original, new Rectangle(0, 0, original.Width, original.Height),
+                0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
+
+            //dispose the Graphics object
+            g.Dispose();
+            return newBitmap;
+        }
+    }
+}
---
+++ La2Bot/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using Tesseract;
+using Rect = Tesseract.Rect;
+
+namespace La2Bot
+{
+    class Program
+    {
+        private static IntPtr _hWnd;
+
+        public static void Main(string[] args)
+        {
+            _hWnd = GetWindowByName("Lineage II");
+            using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+            {
+                while (true)
+                {
+                    var image = Helper.SaveScreenshot(_hWnd);
+                    var src = Mat.FromImageData(ImageToByte2(image));
+                    var blacked = new Mat();
+ 
+                    Cv2.Threshold(src, blacked, 127, 255, ThresholdTypes.Binary);
+
+                    var foundTarget = false;
+                    using (var page = engine.Process(blacked.ToBitmap()))
+                    {
+                        using (var iter = page.GetIterator())
+                        {
+                            iter.Begin();
+                            do
+                            {
+                                if (iter.TryGetBoundingBox(PageIteratorLevel.TextLine, out var rect))
+                                {
+                                    var curText = iter.GetText(PageIteratorLevel.TextLine);
+                                    var modifiedString = curText.Replace("\n", "");
+
+                                    if (modifiedString.Length > 5 && rect.Height > 3 && rect.Height < 20)
+                                    {
+                                        foundTarget = true;
+                                        Console.WriteLine($"{modifiedString}  | {rect.ToString()}");
+                                        Attack(rect);
+                                        break;
+                                    }
+                                }
+                            } while (iter.Next(PageIteratorLevel.TextLine));
+                        }
+                    }
+
+                    if (!foundTarget)
+                        SpinCamera();
+
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr hWnd);
+
+        private static void SpinCamera()
+        {
+            SetForegroundWindow(_hWnd);
+            var rect = new Helper.Rect();
+            GetWindowRect(_hWnd, ref rect);
+
+            SetCursorPos(rect.left + 100, rect.top + 100);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, rect.left + 400, rect.top + 400, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, rect.left + 400, rect.top + 400, 0, 0);
+
+            Thread.Sleep(500);
+
+            for (var i = 0; i < 20; i++)
+            {
+                PostMessage(_hWnd, WM_KEYDOWN, VK_RIGHT, 0);
+                Thread.Sleep(100);
+            }
+        }
+
+        private static IntPtr GetWindowByName(string name)
+        {
+            var hWnd = IntPtr.Zero;
+            foreach (var pList in Process.GetProcesses())
+            {
+                if (pList.MainWindowTitle.Contains(name))
+                {
+                    hWnd = pList.MainWindowHandle;
+                }
+            }
+            return hWnd;
+        }
+
+        private static void Attack(Rect rect)
+        {
+            LeftMouseClick((rect.X1 + rect.X2) / 2, rect.Y1 + 30);
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowRect(IntPtr hWnd, ref Helper.Rect lpRect);
+
+
+        //This is a replacement for Cursor.Position in WinForms
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        private const int MOUSEEVENTF_RIGHTUP = 0x0010;
+
+        const UInt32 WM_KEYDOWN = 0x0100;
+        const UInt32 WM_KEYUP = 0x0101;
+        const int VK_F5 = 0x74;
+        const int VK_RIGHT = 0x27;
+
+        [DllImport("user32.dll")]
+        static extern bool PostMessage(IntPtr hWnd, UInt32 Msg, int wParam, int lParam);
+
+        //This simulates a left mouse click
+        public static void LeftMouseClick(int xpos, int ypos)
+        {
+            var rect = new Helper.Rect();
+            GetWindowRect(_hWnd, ref rect);
+
+            xpos = xpos + rect.left;
+            ypos = ypos + rect.top;
+
+            SetCursorPos(xpos, ypos);
+            Thread.Sleep(200);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+            Thread.Sleep(200);
+            mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+
+            Thread.Sleep(200);
+            SetCursorPos(xpos, ypos);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+            Thread.Sleep(200);
+            mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+
+            Thread.Sleep(200);
+            PostMessage(_hWnd, WM_KEYDOWN, VK_F5, 0);
+            Thread.Sleep(400);
+
+            while (true)
+            {
+                var hp = Helper.GetHp(_hWnd);
+                var pixel = hp.GetPixel(2, 2);
+
+                if(pixel.R > 80)
+                    Console.WriteLine("Alive!");
+                else
+                {
+                    Console.WriteLine("Dead! time to find next target!");
+                    break;                   
+                }
+
+                Thread.Sleep(100);
+            }
+        }
+
+        private static byte[] ImageToByte2(Image img)
+        {
+            using (var stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }      
+    }
+}
