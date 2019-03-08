daca05ee80d960d632c9c57d556486f51e6b037e
SpyGlass <spyglassinyourass@gmail.com>
2019-03-09 01:54:49 +0300

    wip

--- La2Skolopendra/MainTabViewModel.cs
+++ La2Skolopendra/MainTabViewModel.cs
@@ -5,16 +5,13 @@ using JetBrains.Annotations;
 
 namespace La2Skolopendra
 {
-    internal sealed class MainViewModel : ViewModelBase
+    internal sealed class MainTabViewModel : ViewModelBase
     {
         [NotNull] private readonly ILogger _logger;
 
-        [NotNull] public MainTabViewModel MainTabViewModel { get; }
-
-        internal MainViewModel([NotNull] ILogger logger)
+        internal MainTabViewModel([NotNull] ILogger logger)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            MainTabViewModel = new MainTabViewModel(logger);
         }
     }
 }
