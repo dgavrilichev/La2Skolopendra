8770924ca5b38e5f70508459a97f020ccfe31485
SpyGlass <spyglassinyourass@gmail.com>
2019-03-09 02:13:19 +0300

    wip

---
+++ La2Skolopendra/Native/WindowHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using JetBrains.Annotations;
+
+namespace La2Skolopendra.Native
+{
+    internal static class WindowHelper
+    {
+        [DllImport("User32.dll")]
+        internal static extern int SetForegroundWindow(IntPtr hWnd);
+
+        [NotNull]
+        private static IEnumerable<IntPtr> GetWindowsByName(string windowName)
+        {
+            if(string.IsNullOrEmpty(windowName)) throw new ArgumentNullException(nameof(windowName));
+
+            return from pList in Process.GetProcesses()
+                where pList.MainWindowTitle.Contains(windowName)
+                select pList.MainWindowHandle;
+        }
+    }
+}
