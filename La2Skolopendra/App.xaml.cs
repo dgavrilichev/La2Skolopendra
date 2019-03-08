8edde8b9fb4907225b80acae7473276d7c0e6f8c
SpyGlass <spyglassinyourass@gmail.com>
2019-03-09 01:52:33 +0300

    wip

--- La2Skolopendra/App.xaml.cs
+++ La2Skolopendra/App.xaml.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
+using CommonLibrary.Logging;
+using JetBrains.Annotations;
 
 namespace La2Skolopendra
 {
@@ -13,5 +17,22 @@ namespace La2Skolopendra
     /// </summary>
     public partial class App : Application
     {
+        [NotNull]
+        private readonly ILogger _logger = new ThreadSafeLogger(new FileLogger(
+            Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"{DateTime.Today:yy-MM-dd}.txt")));
+
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            _logger.Important("App start");
+            var window = new MainWindow {DataContext = new MainViewModel(_logger)};
+            window.Show();
+        }
+
+        private void AppOnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            _logger.Critical(e.Exception.ToString());
+            MessageBox.Show(e.Exception.ToString(), "Tile Manager - Critical error!", MessageBoxButton.OK, MessageBoxImage.Error);
+            //e.Handled = true;
+        }
     }
 }
--- La2Skolopendra/MainViewModel.cs
+++ La2Skolopendra/MainViewModel.cs
@@ -1,9 +1,17 @@
-﻿using CommonLibrary.Wpf;
+﻿using System;
+using CommonLibrary.Logging;
+using CommonLibrary.Wpf;
+using JetBrains.Annotations;
 
 namespace La2Skolopendra
 {
     internal sealed class MainViewModel : ViewModelBase
     {
+        [NotNull] private readonly ILogger _logger;
 
+        internal MainViewModel([NotNull] ILogger logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
     }
 }
