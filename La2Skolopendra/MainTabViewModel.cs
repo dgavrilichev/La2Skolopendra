3b4a3bc7e6429d71541d88f0573c46d57c558b2e
SpyGlass <spyglassinyourass@gmail.com>
2019-03-09 02:42:53 +0300

    wip

--- La2Skolopendra/MainTabViewModel.cs
+++ La2Skolopendra/MainTabViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using CommonLibrary.Logging;
 using CommonLibrary.Wpf;
 using JetBrains.Annotations;
@@ -17,18 +18,38 @@ namespace La2Skolopendra
 
         [NotNull] public ObservableCollection<La2WindowViewModel> La2WindowsCollection { get; } = new ObservableCollection<La2WindowViewModel>();
 
+        private bool _updateIsEnabled = true;
+        public bool UpdateIsEnabled
+        {
+            get => _updateIsEnabled;
+            set
+            {
+                if (value == _updateIsEnabled) return;
+                _updateIsEnabled = value;
+                NotifyPropertyChanged();
+            }
+        }
+
         internal MainTabViewModel([NotNull] ILogger logger)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
 
-            Task.Run(LoadWindows);
+        public ICommand UpdateCommand
+        {
+            get { return new RelayCommand(o => Task.Run(LoadWindows)); }
         }
 
         private async Task LoadWindows()
         {
+            UpdateIsEnabled = false;
+
             _logger.Info("Now loading La2 windows..");
             var la2Windows = WindowHelper.GetWindowsByName(WindowName).ToList();
-            await Task.Delay(TimeSpan.FromSeconds(0.1));
+            await Task.Delay(TimeSpan.FromSeconds(3));
+
+
+            UpdateIsEnabled = true;
         }
     }
 }
