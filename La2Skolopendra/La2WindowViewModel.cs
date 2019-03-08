aa9a89008cb9062b60c75a6cbf8168cbc0f237bf
SpyGlass <spyglassinyourass@gmail.com>
2019-03-09 02:29:15 +0300

    wip

--- La2Skolopendra/La2WindowViewModel.cs
+++ La2Skolopendra/La2WindowViewModel.cs
@@ -1,8 +1,17 @@
-﻿using CommonLibrary.Wpf;
+﻿using System;
+using System.Windows.Media.Imaging;
+using CommonLibrary.Wpf;
+using JetBrains.Annotations;
 
 namespace La2Skolopendra
 {
     public sealed class La2WindowViewModel : ViewModelBase
     {
+        public BitmapSource Image { get; }
+
+        internal La2WindowViewModel([NotNull] BitmapSource bitmapSource)
+        {
+            Image = bitmapSource ?? throw new ArgumentNullException(nameof(bitmapSource));
+        }
     }
 }
