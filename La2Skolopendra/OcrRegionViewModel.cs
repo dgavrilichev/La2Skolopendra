27420de48fb7436db2398c39f88cf6571d96e22b
SpyGlass <spyglassinyourass@gmail.com>
2019-03-09 12:26:13 +0300

    wip

--- La2Skolopendra/OcrRegionViewModel.cs
+++ La2Skolopendra/OcrRegionViewModel.cs
@@ -1,9 +1,31 @@
-﻿using CommonLibrary.Wpf;
+﻿using System;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using CommonLibrary;
+using CommonLibrary.Wpf;
+using JetBrains.Annotations;
 
 namespace La2Skolopendra
 {
     public sealed class OcrRegionViewModel : ViewModelBase
-    { 
+    {
+        private Bitmap _mainWindowImage;
 
+        private BitmapSource _image;
+        public BitmapSource Image
+        {
+            get => _image;
+            set
+            {
+                _image = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        internal void SetMainWindowImage([NotNull] Bitmap mainWindowImage)
+        {
+            _mainWindowImage = mainWindowImage ?? throw new ArgumentNullException(nameof(mainWindowImage));
+            Image = BitmapHelper.BitmapToBitmapSource(mainWindowImage);
+        }
     }
 }
