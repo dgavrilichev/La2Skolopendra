7f165f8bfc22b98bbe83790549a073816558aa16
SpyGlass <spyglassinyourass@gmail.com>
2019-03-09 17:44:48 +0300

    wip

--- La2Skolopendra/OcrRemovableSelectorViewModel.cs
+++ La2Skolopendra/OcrRemovableSelectorViewModel.cs
@@ -1,6 +1,39 @@
-﻿namespace La2Skolopendra
+﻿using System;
+using System.Drawing;
+using CommonLibrary.Wpf;
+
+namespace La2Skolopendra
 {
-    internal sealed class OcrRemovableSelectorViewModel
+    internal sealed class OcrRemovableSelectorViewModel : ViewModelBase
     {
+        internal event EventHandler<Rectangle> AreaBoundsChanged;
+        private void OnAreaBoundsChanged(Rectangle e)
+        {
+            AreaBoundsChanged?.Invoke(this, e);
+        }
+
+        internal event EventHandler RequestRemove;
+        private void OnRequestRemove()
+        {
+            RequestRemove?.Invoke(this, EventArgs.Empty);
+        }
+
+        private OcrAreaSelectorViewModel _selectorViewModel;
+        public OcrAreaSelectorViewModel SelectorViewModel
+        {
+            get => _selectorViewModel;
+            set
+            {
+                if (value == _selectorViewModel) return;
+                _selectorViewModel = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        internal OcrRemovableSelectorViewModel()
+        {
+            SelectorViewModel = new OcrAreaSelectorViewModel();
+            SelectorViewModel.AreaBoundsChanged += (sender, rectangle) => OnAreaBoundsChanged(rectangle);
+        }
     }
 }
