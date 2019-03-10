d507d16106b1c02d3e8dbe5512d37f2301335686
SpyGlass <spyglassinyourass@gmail.com>
2019-03-10 14:53:08 +0300

    wip

--- La2Skolopendra/Engine/MasterProcessor.cs
+++ La2Skolopendra/Engine/MasterProcessor.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using CommonLibrary;
+using System.Windows.Documents;
 using JetBrains.Annotations;
 using La2Skolopendra.Export;
 using La2Skolopendra.Native;
 using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using Tesseract;
+using BitmapHelper = CommonLibrary.BitmapHelper;
+using Point = System.Drawing.Point;
 
 namespace La2Skolopendra.Engine
 {
@@ -57,6 +63,51 @@ namespace La2Skolopendra.Engine
             var src = Mat.FromImageData(BitmapHelper.ImageToByte2(screenshotWithExclude));
             var blacked = new Mat();
             Cv2.Threshold(src, blacked, 127, 255, ThresholdTypes.Binary);
-        } 
+
+            var blackedBitmap = blacked.ToBitmap();
+
+            var targets = GetTargets(blackedBitmap);
+        }
+
+        private List<Point> GetTargets([NotNull] Bitmap blacked)
+        {
+            if(blacked == null) throw new ArgumentNullException(nameof(blacked));
+
+            var result = new List<Point>();
+            var log = string.Empty;
+
+            using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+            {
+                using (var page = engine.Process(blacked))
+                {
+                    using (var iterator = page.GetIterator())
+                    {
+                        iterator.Begin();
+                        do
+                        {
+                            if (iterator.TryGetBoundingBox(PageIteratorLevel.TextLine, out var rect))
+                            {
+                                var curText = iterator.GetText(PageIteratorLevel.TextLine);
+                                var modifiedString = curText.Replace("\n", "");
+
+                                if (modifiedString.Length > 4 && rect.Height > 3 && rect.Height < 20)
+                                {
+
+                                    log = $"{log}\n{modifiedString} | {rect.ToString()}";
+                                    result.Add(new Point((rect.X1 + rect.X2) / 2, rect.Y1 + 30));
+                                }
+                            }
+                        } while (iterator.Next(PageIteratorLevel.TextLine));
+                    }
+                }
+            }
+
+            log = result.Any() 
+                ? $"Found possible targets ({result.Count}): {log}" 
+                : $"Targets not found";
+            OnReport(log);
+
+            return result;
+        }
     }
 }
